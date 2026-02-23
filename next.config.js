2d6ad9c02a81dd5266fba8e4919a2d2a21015deb
meditationastro <meditationastroweb@gmail.com>
2026-02-23 22:22:45 +0545

    dd

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
-
-
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
   images: {
     domains: [
--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
-
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  DATABASE_URL='postgresql://neondb_owner:npg_uzJ7d3PtZMVW@ep-shy-sun-a1iturt3-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'
 };
 
 export default nextConfig;
